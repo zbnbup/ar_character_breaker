0f6974b4ea567a4c183f0300e732773136558823
zbnbup <51353544+zbnbup@users.noreply.github.com>
2019-06-22 18:51:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
+
+promise.then(successCallback)
+       .then(errorCallback);
+
+function successCallback(stream){
+  video.srcObject = stream;
+  };
+
+function errorCallback(err){
+  alert(err);
+  };
+
 const worker = new Tesseract.TesseractWorker();
 worker
   .recognize(myImage)
